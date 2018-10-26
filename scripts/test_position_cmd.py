567df74a3f5396203b9a0d570093d99791658bec
Anas Abou Allaban <piraka9011@hotmail.com>
2018-10-26 11:01:25 +0300

    Ready for position control

---
+++ scripts/test_position_cmd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import JointState
+
+if __name__ == '__main__':
+    rospy.init_node('test_position_cmd')
+    pub = rospy.Publisher('/turtlebot_arm/position_cmd', JointState, queue_size=10)
+    while pub.get_num_connections() == 0:
+        rospy.Rate(5).sleep()
+
+    j = JointState()
+    j.name = ['joint_1', 'joint_2', 'joint_3', 'gripper']
+    j.position = [512, 0, 0, 0]
+    j.header.stamp = rospy.Time().now()
+    pub.publish(j)
+    rospy.loginfo("Fin.")
--- src/arm_control.py
+++ src/arm_control.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import sys
 import serial
 import thread
+from threading import Thread
 
 import rospy
 from dynamixel_constants import *
 from dynamixel_workbench_msgs.msg import DynamixelState, DynamixelStateList
-from sensor_msgs.msg import  JointState
+from sensor_msgs.msg import JointState
 
 
 class ArmControl:
@@ -18,6 +18,7 @@ class ArmControl:
         self.error = 0
         self.servo_count = 0
         self._mutex = thread.allocate_lock()
+        self._joint_names = ['joint_1', 'joint_2', 'joint_3', 'gripper']
         self._baud_codes = {1000000: 1,
                             500000: 3,
                             400000: 4,
@@ -29,10 +30,13 @@ class ArmControl:
                             9600: 207}
         rospy.logdebug("Params:\n"
                        "Port: {}\nBaud: {}\n".format(self.port, self.baud))
+
         # Serial setup
         try:
-            self._ser = serial.Serial(port=self.port, baudrate=self.baud, timeout=0.1)
-            rospy.loginfo("Arbotix Connected! Connection is {}".format(self._ser.is_open))
+            self._ser = serial.Serial(port=self.port, baudrate=self.baud, timeout=0.25)
+            while not self._ser.isOpen():
+                rospy.Rate(5).sleep()
+            rospy.loginfo("Arbotix Connected!")
         except serial.SerialException as e:
             self._ser.close()
             rospy.logwarn("Got a Serial exception!\n{}".format(e))
@@ -41,13 +45,10 @@ class ArmControl:
         self._dyna_state_pub = rospy.Publisher('/turtlebot_arm/dynamixel_state', DynamixelStateList, queue_size=10)
         self._joint_state_pub = rospy.Publisher('/turtlebot_arm/joint_state', JointState, queue_size=10)
         rospy.Subscriber('/turtlebot_arm/position_cmd', JointState, self._position_cmd_cb)
-        self._arm_joint_state = JointState()
-        self._arm_joint_state.name = ['joint_1', 'joint_2', 'joint_3', 'gripper']
 
     def __del__(self):
         rospy.loginfo("Closing Arbotix serial connection...")
         self._ser.close()
-
 
     # ======================================================= #
     #           Serial Connection Utility Functions           #
@@ -59,37 +60,25 @@ class ArmControl:
             self._mutex.release()
             raise serial.SerialException(e)
 
-    def openPort(self):
-        self._ser.close()
-        try:
-            self._ser.open()
-        except serial.SerialException as e:
-            raise serial.SerialException(e)
-
-    def closePort(self):
-        self._ser.close()
-
-    def getPacket(self, mode, id=-1, leng=-1, error=-1, params=None):
+    def getPacket(self, mode, servo_id=-1, length=-1, error=-1, params=None):
         """ Read a dynamixel return packet in an iterative attempt.
-            Parameters
-            ----------
-            mode : int
-                This should be 0 to start reading packet.
-            Returns
-            -------
-            packet : int
-                The error level returned by the device.
+            :param mode: This should be 0 to start reading packet.
+            :param servo_id: The servo ID to read.
+            :param length: The length of the packet size (bytes).
+            :param error: The error level to return.
+            :param params: Parameters to set, if any.
+            :return: The error level returned by the device.
         """
         try:
             d = self._ser.read()
         except Exception as e:
             print e
             return None
-        # need a positive byte
+            # need a positive byte
         if d == '':
             return None
 
-        # now process our byte
+            # now process our byte
         if mode == 0:  # get our first 0xFF
             if ord(d) == 0xff:
                 return self.getPacket(1)
@@ -106,25 +95,25 @@ class ArmControl:
             else:
                 return self.getPacket(0)
         elif mode == 3:  # get length
-            return self.getPacket(4, id, ord(d))
+            return self.getPacket(4, servo_id, ord(d))
         elif mode == 4:  # read error
             self.error = ord(d)
-            if leng == 2:
-                return self.getPacket(6, id, leng, ord(d), list())
+            if length == 2:
+                return self.getPacket(6, servo_id, length, ord(d), list())
             else:
-                return self.getPacket(5, id, leng, ord(d), list())
+                return self.getPacket(5, servo_id, length, ord(d), list())
         elif mode == 5:  # read params
             params.append(ord(d))
-            if len(params) + 2 == leng:
-                return self.getPacket(6, id, leng, error, params)
+            if len(params) + 2 == length:
+                return self.getPacket(6, servo_id, length, error, params)
             else:
-                return self.getPacket(5, id, leng, error, params)
+                return self.getPacket(5, servo_id, length, error, params)
         elif mode == 6:  # read checksum
-            checksum = id + leng + error + sum(params) + ord(d)
+            checksum = servo_id + length + error + sum(params) + ord(d)
             if checksum % 256 != 255:
                 return None
             return params
-        # fail
+            # fail
         return None
 
     def execute(self, index, ins, params, ret=True):
@@ -180,15 +169,15 @@ class ArmControl:
             @param start The starting register address to begin writing to.
             @param values The data to write, in a list.
             @return The error level.
-            """
+        """
         self.execute(index, AX_WRITE_DATA, [start] + values)
         return self.error
 
     def syncWrite(self, start, values):
-        """Write values to registers on many servos.
-            @param start The starting register address to begin writing to.
-            @param values The data to write, in a list of lists. Format should be
-                   [(id1, val1, val2), (id2, val1, val2)]
+        """ Write values to registers on many servos.
+            :param start: The starting register address to begin writing to.
+            :param values: The data to write, in a list of lists. Format should be
+                           [(id1, val1, val2), (id2, val1, val2)]
         """
         output = list()
         for i in values:
@@ -225,26 +214,50 @@ class ArmControl:
     def _val2rad(self, value):
         return (value - AX_ZERO_RAD_POS) * AX_MAX_RAD / (AX_MAX_RAD_POS - AX_ZERO_RAD_POS)
 
-    def _
+    def _val2vel(self, value):
+        return value / AX_VEL_VAL_RATIO
 
     # ========================================= #
     #           ROS Utility Functions           #
     # ========================================= #
     def _position_cmd_cb(self, msg):
-        pass
+        position_packet = []
+        for servo_id in range(1, self.servo_count + 1):
+            try:
+                # Servo IDs are 1-indexed
+                goal_pos = msg.position[servo_id - 1]
+                # See syncWrite for format
+                servo_packet = [servo_id, int(goal_pos) % 256, int(goal_pos) >> 8]
+                position_packet.append(servo_packet)
+            except IndexError:
+                rospy.logwarn("Number of servo's connected ({}) does not equal number of"
+                              "joints! Check definition of JointState Message\nJoint State Names: {}"
+                              .format(self.servo_count, msg.name))
+
+        # Write to multiple servos
+        self.syncWrite(P_GOAL_POSITION_L, position_packet)
 
     # ========================================== #
     #           Controller set/getters           #
     # ========================================== #
     def setBaudRate(self, servo_id=1, baud=1000000):
+        """ Set the baud rate of a specific servo.
+            :param servo_id: The ID of the device to write.
+            :param baud: The baud rate. Make sure its available.
+            :return: The error level as int.
+        """
         try:
-            self.write(servo_id, P_BAUD_RATE, [self._baud_codes[baud]])
+            return self.write(servo_id, P_BAUD_RATE, [self._baud_codes[baud]])
         except KeyError:
-            rospy.logerr("Please enter a valid baud rate! Setting to 1Mbps.")
-            self.write(servo_id, P_BAUD_RATE, [1])
+            rospy.logerr("Please enter a valid baud rate! Setting to default 1 Mbps.")
+            return self.write(servo_id, P_BAUD_RATE, [1])
 
     def setPosition(self, servo_id=1, value=510):
-        # Set limits 60 - 820
+        """ Set the position of a specific servo. Limited to 60-820 for safety.
+            :param servo_id: The ID of the device to write.
+            :param value: The value of the servo position. Choose from 0-1023
+            :return: The error level as int.
+        """
         if value < 60:
             value = 60
         elif value > 820:
@@ -252,110 +265,187 @@ class ArmControl:
 
         return self.write(servo_id, P_GOAL_POSITION_L, [value % 256, value >> 8])
 
+    def setVelocity(self, servo_id=1, value=1):
+        """ Set the speed of a servo. Units are rpm. Increments of 0.111rpm
+            :param servo_id: The ID of the device to write.
+            :param value: The velocity to write.
+            :return: The error level as int.
+        """
+        if value < 0:
+            value = 0
+        elif value > 1023:
+            value = 1023
+        return self.write(servo_id, P_GOAL_SPEED_L, [value % 256, value >> 8])
+
     def getModelName(self, servo_id=1):
+        """ Get the model name of the servo.
+            :param servo_id: The ID of the device to read.
+            :return: The servo's model name as a string.
+        """
         try:
             model_num = int(self.read(servo_id, P_MODEL_NUMBER_L, 1)[0])
             return MODEL_NAME[model_num]
         except KeyError:
             rospy.logerr("Model {} does not exist!".format(model_num))
             return -1
-
-    def getTorqueEnable(self, servo_id=1):
-        return self.read(servo_id, P_TORQUE_ENABLE, 1)[0]
+        except TypeError:
+            return -1
 
     def getGoalVelocity(self, servo_id=1):
-        """Get the goal velocity of a servo.
-        :param servo_id: The ID of the device to read.
-        :return: The servo's goal velocity.
+        """ Get the goal velocity of a servo.
+            :param servo_id: The ID of the device to read.
+            :return: The servo's goal velocity.
         """
         values = self.read(servo_id, P_PRESENT_SPEED_L, 2)
         try:
             return int(values[0]) + (int(values[1]) << 8)
-        except:
+        except TypeError:
             return -1
 
     def getGoalPosition(self, servo_id=1):
-        """Get the goal position of a servo.
-        :param servo_id: The ID of the device to read.
-        :return: The servo's goal position.
+        """ Get the goal position of a servo.
+            :param servo_id: The ID of the device to read.
+            :return: The servo's goal position.
         """
         values = self.read(servo_id, P_GOAL_POSITION_L, 2)
         try:
             return int(values[0]) + (int(values[1]) << 8)
-        except:
+        except TypeError:
             return -1
 
-    def getPresentVelocity(self, servo_id):
-        """Get the current velocity of a servo.
-        :param servo_id: The ID of the device to read.
-        :return: The servo's current velocity.
+    def getPresentVelocity(self, servo_id=1):
+        """ Get the current velocity of a servo.
+            :param servo_id: The ID of the device to read.
+            :return: The servo's current velocity.
         """
         values = self.read(servo_id, P_GOAL_SPEED_L, 2)
         try:
             return int(values[0]) + (int(values[1]) << 8)
-        except:
+        except TypeError:
             return -1
 
-    def getPresentPosition(self, servo_id):
-        """Get the current position of a servo.
-        :param servo_id: The ID of the device to read.
-        :return: The servo's current position.
+    def getPresentPosition(self, servo_id=1):
+        """ Get the current position of a servo.
+            :param servo_id: The ID of the device to read.
+            :return: The servo's current position.
         """
         values = self.read(servo_id, P_PRESENT_POSITION_L, 2)
         try:
             return int(values[0]) + (int(values[1]) << 8)
-        except:
+        except TypeError:
             return -1
 
     def getTemperature(self, servo_id=1):
-        """Get the temperature of a device.
-        :param servo_id: The ID of the device to read.
-        :return: The temperature, in degrees C.
+        """ Get the temperature of a device.
+            :param servo_id: The ID of the device to read.
+            :return: The temperature, in degrees C.
         """
         try:
             return int(self.read(servo_id, P_PRESENT_TEMPERATURE, 1)[0])
-        except:
+        except TypeError:
             return -1
 
-    def getServosCount(self, max_id=10):
+    def getServosCount(self, max_id=5, retry=3):
+        """ Get the number of servos connected.
+            :param max_id: Maximum number of servos to look for.
+            :param retry: Number of times to retry looking for servos.
+            :return: The number of servos found.
+        """
         id_count = 0
-        self._ser.timeout = 0.25
-        for i in range(max_id):
-            if self.getPosition(i+1) != -1:
-                id_count += 1
+        retry_count = 0
+        while retry_count <= retry:
+            # Check if servo accesible by reading positions
+            for i in range(max_id):
+                if self.getPresentPosition(i + 1) != -1:
+                    id_count += 1
+            # Check if we want to retry
+            if id_count == 0 and retry != 0:
+                rospy.logwarn("No Servos found! Retrying...")
+                retry_count += 1
+                rospy.sleep(1)
+            else:
+                break
 
         self.servo_count = id_count
-        rospy.loginfo("Got {} servos".format(self.servo_count))
         return self.servo_count
 
+    # ===================================== #
+    #           Controller Checks           #
+    # ===================================== #
+
+    def isTorqueEnabled(self, servo_id=1):
+        """ Check whether torque is enabled or not.
+            :param servo_id: The ID of the device to read.
+            :return: Torque enabled (1) or not (0)
+        """
+        try:
+            return self.read(servo_id, P_TORQUE_ENABLE, 1)[0]
+        except TypeError:
+            return -1
+
+    def isMoving(self, servo_id=1):
+        """ Check if servo is executing a command or not.
+            :param servo_id: The ID of the device to read.
+            :return: Servo is moving (1) or not (0).
+        """
+        try:
+            return self.read(servo_id, P_MOVING, 1)[0]
+        except TypeError:
+            return -1
+
+    # ======================================== #
+    #           Controller Functions           #
+    # ======================================== #
+
     def publishDynamixelState(self):
-        dyn_state_list = DynamixelStateList()
+        """ Publish current state of all Dynamixel Servos as a DynamixelStateList message. """
+        while not rospy.is_shutdown():
+            dyn_state_list = DynamixelStateList()
+            for servo_id in range(1, self.servo_count + 1):
+                dyn_state = DynamixelState()
+                dyn_state.model_name = self.getModelName(servo_id)
+                dyn_state.id = servo_id
+                dyn_state.torque_enable = self.isTorqueEnabled(servo_id)
+                dyn_state.present_position = self.getPresentPosition(servo_id)
+                dyn_state.present_velocity = self.getPresentVelocity(servo_id)
+                dyn_state.goal_position = self.getGoalPosition(servo_id)
+                dyn_state.goal_velocity = self.getGoalVelocity(servo_id)
+                dyn_state.moving = self.isMoving(servo_id)
+                dyn_state_list.dynamixel_state.append(dyn_state)
 
-        for servo_id in range(1, self.servo_count+1):
-            dyn_state = DynamixelState()
-            dyn_state.model_name = self.getModelName(servo_id)
-            dyn_state.id = servo_id
-            dyn_state.torque_enable = self.getTorqueEnable(servo_id)
-            dyn_state.present_position = self.getPresentPosition(servo_id)
-            dyn_state.present_velocity = self.getPresentVelocity(servo_id)
-            dyn_state.goal_position = self.getGoalPosition(servo_id)
-            dyn_state.goal_velocity = self.getGoalVelocity(servo_id)
-            dyn_state_list.append(dyn_state)
-
-        self._dyna_state_pub.publish(dyn_state_list)
+            self._dyna_state_pub.publish(dyn_state_list)
+            rospy.Rate(10).sleep()
 
     def publishJointState(self):
-        for servo_id in range(1, self.servo_count + 1):
-            curr_pos = self.getPresentPosition(servo_id)
-            curr_vel = self.getPresentVelocity(servo_id)
-            self._arm_joint_state.position[servo_id] = self._val2rad(curr_pos)
-            self._arm_joint_state.velocity[servo_id] = self._val2vel(curr_vel)
+        """ Publish current servo positions/velocities as a JointState message. """
+        while not rospy.is_shutdown():
+            curr_state = JointState()
+            curr_state.name = self._joint_names
+            for servo_id in range(1, self.servo_count + 1):
+                curr_pos = self.getPresentPosition(servo_id)
+                curr_vel = self.getPresentVelocity(servo_id)
+                # Servo's are 1-indexed
+                curr_state.position.append(self._val2rad(curr_pos))
+                curr_state.velocity.append(self._val2vel(curr_vel))
+
+            curr_state.header.stamp = rospy.Time().now()
+            self._joint_state_pub.publish(curr_state)
+            rospy.Rate(10).sleep()
+
+    def start(self):
+        self.getServosCount()
+        rospy.loginfo("Got {} Servos".format(self.servo_count))
+        dyn_state_thread = Thread(target=self.publishDynamixelState, name="Dynamixel State Publisher")
+        dyn_state_thread.start()
+        rospy.loginfo("Publishing Dynamixel states...")
+        joint_state_thread = Thread(target=self.publishJointState, name="Joint State Publisher")
+        joint_state_thread.start()
+        rospy.loginfo("Publishing joint states...")
+        rospy.loginfo("Spinning...")
+        rospy.spin()
 
 
 if __name__ == '__main__':
-    rospy.init_node("arbotix_test", log_level=rospy.DEBUG)
+    rospy.init_node("arbotix_test")
     arb = ArmControl()
-    arb.getServosCount()
-    # arb.setPosition()
-
-
+    arb.start()
